0ffa23fca28c6824e7e9c0c97c0290679e091092
Aryanjast13 <aryanjastaj05@gmail.com>
2025-05-01 11:54:00 +0530

    update

--- backend/src/utils/uploadToS3.ts
+++ backend/src/utils/uploadToS3.ts
@@ -12,25 +12,5 @@ const upload = multer({ storage });
 
 
 export default upload;
-// Endpoint to handle image upload
-// app.post(
-//   "/api/upload",
-//   upload.single("image"),
-//   (req: Request, res: Response) => {
-//     if (!req.file) {
-//       return res
-//         .status(400)
-//         .json({ success: false, message: "No file uploaded." });
-//     }
-
-//     const imageUrl = req.file.location;
-
-//     return res.json({
-//       success: true,
-//       message: "Image uploaded successfully!",
-//       imageUrl: imageUrl, // URL of the uploaded image in S3
-//     });
-//   }
-// );
 
 
